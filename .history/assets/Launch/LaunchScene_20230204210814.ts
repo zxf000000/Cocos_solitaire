3280b96041e3ab2b3d259cab08bd6514db72fde0
butcher-blaviken <butcher_of_blavikennnn@outlook.com>
2023-02-04 21:22:41 +0800

    create launch scene

---
+++ .history/assets/Launch/LaunchScene_20230204210814.ts
@@ -0,0 +1,10 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Launch')
+export class Launch extends Component {
+    start() {
+        conso
+    }
+}
+
---
+++ .history/assets/Launch/LaunchScene_20230204210855.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node, EventMouse } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Launch')
+export class Launch extends Component {
+    start() {
+        console.info("Start LaunchScene");
+    }
+
+    private handleClickButton(event: EventMouse) {
+        console.info("")
+    }
+}
+
